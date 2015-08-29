806507422b500b68c6a63bc74df11c75fc1a099d
mrg219 <mrg219@lehigh.edu>
2015-08-29 18:21:07 +0000

    update WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,17 @@
+// Max Grossman
+// CSE002
+// 8-29-15
+// WelcomeClass Java Program
+public class WelcomeClass{
+    public static void main(String[] args) {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-M--R--G--2--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("Hi, I'm Max. I'm From Louisville, Kentucky and like to play soccer and lacrosse.");
+}
+}
